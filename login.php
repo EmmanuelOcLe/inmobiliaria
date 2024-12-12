dc747c413d4ffa70dbafd2235ea9b4625d709efa
EmmanuelOcLe <179487005+EmmanuelOcLe@users.noreply.github.com>
2024-12-12 10:58:48 -0500

    fix login.php

--- login.php
+++ login.php
@@ -5,34 +5,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/global.css">
     <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="icon" href="assets/favicon.ico">
     <title>Inmobiliaria Emmanuel</title>
 </head>
 <body>
-    <div class="contenedor-todo">
-        <?php include('header.php'); ?>
+    <?php include('header.php');?>
 
-        <main>
-            <div class="container">
-                    <form class="login-form">
-                        <h2>LOGIN</h2>
-                        <div class="input-group">
-                            <label for="correo">Correo</label>
-                            <input type="email" id="correo" name="correo" required>
-                        </div>
-                        <div class="input-group">
-                            <label for="contrasena">Contraseña</label>
-                            <input type="password" id="contrasena" name="contrasena" required>
-                        </div>
-                        <div class="button-group">
-                            <button type="button" class="btn-cancel">Cancelar</button>
-                            <button type="submit" class="btn-submit">Ingresar</button>
-                        </div>
-                    </form>
+    <main class="main-tag">
+        <div class="container">
+            <form class="login-form">
+                <h2>LOGIN</h2>
+                <div class="input-group">
+                    <label for="correo">Correo</label>
+                    <input type="email" id="correo" name="correo" required>
                 </div>
+                <div class="input-group">
+                    <label for="contrasena">Contraseña</label>
+                    <input type="password" id="contrasena" name="contrasena" required>
+                </div>
+                <div class="button-group">
+                    <button type="button" class="btn-cancel">Cancelar</button>
+                    <button type="submit" class="btn-submit">Ingresar</button>
+                </div>
+            </form>
+        </div>
+    </main>
 
-        </main>
+    <?php include('footer.php'); ?>
 
-        <?php include('footer.php'); ?>
     </div>
 
 </body>
