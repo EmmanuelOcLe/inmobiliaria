b5cdd99042cbb955f87c00b1178a7afcd31d3194
Eileen Lidueñez <eileenlc0509@gmail.com>
2024-12-11 23:22:54 -0500

    cambio de archivos a php

---
+++ header.php
@@ -0,0 +1,14 @@
+<header>
+    <div class="logo">IE</div>
+    <div class="nav-button">
+
+        <nav class="navbar">
+            <a href="">Inicio</a>
+            <a href="">Sobre Nosotros</a>
+            <a href="">Servicios</a>
+        </nav>
+
+        <button class="header-button" onclick="window.location.href='politicas.html'">Administración</button>
+    </div>   
+</header>
+    
