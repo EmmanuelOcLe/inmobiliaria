f484c833e2e3f0dd167384101fbdfb76c416e39f
michael-myers-myke <zuluagamaikol1@gmail.com>
2024-12-13 01:08:27 -0500

    add back registro form

--- back/properties/create.php
+++ back/properties/create.php
@@ -2,23 +2,23 @@
 session_start();
 require_once("conection.php");
  
-$sql="insert into propiedades ( fecha_inicio, fecha_finalizacion, nombre, descripcion, tipo_contrato, pago, valor) 
+$sql="insert into inmueble( nombre_inmueble, ubicacion_inmueble, precio_inmueble, cantidad_habitaciones, cantidad_baños, zona_parqueo, area, fotos_inmueble, Descripcion_inmueble)
 values
-('".$_POST["fecha_inicio"]."','".$_POST["fecha_finalizacion"]."','".$_POST["nombre"]."','".$_POST["descripcion"]."','".$_POST["tipo_contrato"]."','".$_POST["pago"]."','".$_POST["valor"]."')";
+('".$_POST["propiedad_nombre"]."','".$_POST["ubicacion_propiedad"]."','".$_POST["valor_propiedad"]."','".$_POST["habitaciones_cantidad"]."','".$_POST["baños_cantidad"]."','".$_POST["zona_parqueo"]."','".$_POST["areas_metros"]."', '".$_POST["imagen"]"','".$_POST["descripcion"]"')";
 
 $res=mysqli_query($con,$sql);
 
 if ($res == 1)
 {
 		echo "<script type='text/javascript'>
-		alert('Fue Ingresado Correctamente');
+		alert('Fue creado Correctamente');
 	</script>";
 }else
 {
 
 	
 	echo "<script type='text/javascript'>
-		alert('No Fue Ingresado, Intente Nuevamente');
+		alert('No Fue creado, Intente Nuevamente');
 	</script>";
 }
 ?>
