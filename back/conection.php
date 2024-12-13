3463bea744fd5781aa763e45a41c0a0ccdacb6cc
Eileen Lidueñez <eileenlc0509@gmail.com>
2024-12-13 11:24:38 -0500

    arreglo de conexion

--- back/conection.php
+++ back/conection.php
@@ -1,12 +1,13 @@
 <?php
-$host = 'localhost'; 
-$user = 'root'; 
-$password = ''; 
-$database = 'inmobiliaria';
+$con = mysqli_connect("localhost", "root", "123456", "bienesraices");
+$host = "localhost";
+$user = "root";
+$password = "123456";
+$dbname = "inmobiliaria"; 
 
-$conn = new mysqli($host, $user, $password, $database);
+$conn = new mysqli($host, $user, $password, $dbname);
 
 if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+    die("Conexión fallida: " . $conn->connect_error);
 }
 ?>
