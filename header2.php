60354bfc1de1b1209a11e8bc27f5db14d8182b56
Eileen Lidueñez <eileenlc0509@gmail.com>
2024-12-14 21:05:24 -0500

    arreglo de diseño de navbar

--- header2.php
+++ header2.php
@@ -1,14 +1,18 @@
+
+
+
 <header>
-    <div class="logo">
-        <a href="dashboard-admin.php">IE</a>
-    </div>
-    <div class="nav-button">
-
-        <nav class="navbar">
-            <a href="dashboard-admin.php">Dashboard</a>
-            <a href="inhabilitadas.php">Inmuebles Inhabilitados</a>
-        </nav>
-
-        <button class="header-button-red" onclick="window.location.href='back/logout.php'">Cerrar Sesion</button>
-    </div>   
+    <nav class="navbar">
+        <a href="index.php" class="header-logo-link">
+        <h1 class="header-logo">IE</h1>
+        </a>
+        
+        <div class="header-options-container">
+            <a href="index.php" class="header-option">Inicio</a>
+            <a href="about.php" class="header-option">Dashboard</a>
+            <a href="arrendamiento.php" class="header-option">Inmuebles Inhabilitados</a>
+            <a href="login.php" class="header-option-logout" onclick="window.location.href='back/logout.php'">Cerrar Sesion</a>
+        </div>
+    </nav>  
 </header>
+    
