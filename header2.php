c6300350931a5b955424e73a78ec7f6bde576e1f
Eileen Lidueñez <eileenlc0509@gmail.com>
2024-12-14 21:41:16 -0500

    actualizacion de navbar

--- header2.php
+++ header2.php
@@ -1,6 +1,3 @@
-
-
-
 <header>
     <nav class="navbar">
         <a href="index.php" class="header-logo-link">
@@ -9,9 +6,9 @@
         
         <div class="header-options-container">
             <a href="index.php" class="header-option">Inicio</a>
-            <a href="about.php" class="header-option">Dashboard</a>
-            <a href="arrendamiento.php" class="header-option">Inmuebles Inhabilitados</a>
-            <a href="login.php" class="header-option-logout" onclick="window.location.href='back/logout.php'">Cerrar Sesion</a>
+            <a href="dashboard-admin.php" class="header-option">Dashboard</a>
+            <a href="inhabilitadas.php" class="header-option">Inmuebles Inhabilitados</a>
+            <a href="back/logout.php" class="header-option-logout">Cerrar Sesion</a>
         </div>
     </nav>  
 </header>
