38620cc0ed268df74daa189e27c1a3394b17b1a8
ValleDeve <vallebarbaranj@gmail.com>
2024-12-15 20:09:07 -0500

    advances

--- view-property.php
+++ view-property.php
@@ -130,7 +130,7 @@
                 class="slider-image"
                 id="propertyImage"
               />
-              <button class="slider-button prev" onclick="changeImage(-1)">
+              <button class="slider-button prev" type="reset" onclick="changeImage(-1)">
                 ←
               </button>
               <button class="slider-button next" onclick="changeImage(1)">→</button>
